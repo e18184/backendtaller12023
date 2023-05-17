9f625682502bab59572b0187e39942364b784e04
Richard Sivila <e18184.com>
2023-05-17 14:48:58 -0400

    17-05-2023

---
+++ src/main/java/com/example/demo/taller1proyecto/Taller1proyectoApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo.taller1proyecto;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Taller1proyectoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Taller1proyectoApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/example/demo/taller1proyecto/modelo/Menus.java
@@ -0,0 +1,28 @@
+package com.example.demo.taller1proyecto.modelo;
+
+import java.io.Serializable;
+import java.util.HashSet;
+import java.util.Set;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import jakarta.persistence.ManyToMany;
+import lombok.Data;
+
+@Data
+@Entity
+public class Menus implements Serializable {
+    @Id
+    @Column(name = "codm")
+    @GeneratedValue(generator = "MENUS_CODM")
+
+    Integer codm;
+    @Column(name = "nombre", length = 30)
+    String nombre;
+    Integer estado;
+    @ManyToMany
+    Set<Procesos> procesos = new HashSet<Procesos>();
+
+}
