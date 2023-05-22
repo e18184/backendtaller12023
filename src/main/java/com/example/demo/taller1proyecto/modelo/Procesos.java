64100d1a387c6f11d18d730ee3059b812d9cbac5
Richard Sivila <e18184.com>
2023-05-22 15:17:18 -0400

    22-05-2023

--- src/main/java/com/example/demo/taller1proyecto/modelo/Procesos.java
+++ src/main/java/com/example/demo/taller1proyecto/modelo/Procesos.java
@@ -9,7 +9,6 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Id;
 import jakarta.persistence.ManyToMany;
-import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
