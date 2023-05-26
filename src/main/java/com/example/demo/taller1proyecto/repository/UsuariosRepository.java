b1787bacc69e0f95ed00c8ddbc6709169e49b185
Richard Sivila <e18184.com>
2023-05-26 14:58:05 -0400

    26-05-2023 14:57

--- src/main/java/com/example/demo/taller1proyecto/repository/UsuariosRepository.java
+++ src/main/java/com/example/demo/taller1proyecto/repository/UsuariosRepository.java
@@ -1,7 +1,6 @@
 package com.example.demo.taller1proyecto.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
 import com.example.demo.taller1proyecto.modelo.Usuarios;
 
 public interface UsuariosRepository extends JpaRepository<Usuarios, Long> {
