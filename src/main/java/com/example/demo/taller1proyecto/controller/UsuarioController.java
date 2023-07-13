23e88d7f0feecf842eb49fe669b9b58f2154fb1e
e18184@gmail.com <e18184@gmail.com>
2023-07-13 14:01:40 -0400

    12-07-2023 apirest

--- src/main/java/com/example/demo/taller1proyecto/controller/UsuarioController.java
+++ src/main/java/com/example/demo/taller1proyecto/controller/UsuarioController.java
@@ -19,7 +19,7 @@ public class UsuarioController {
     @GetMapping()
     public ResponseEntity<List<Usuarios>> findAll() {
         try {
-            // esto es una prueba
+            // esto es una pruebas
             List<Usuarios> usuario = new ArrayList<Usuarios>();
             usuarioService.findAll().forEach(usuario::add);
             return new ResponseEntity<>(usuario, HttpStatus.OK);
