430a364e9d7582fdf07b67167e249566c8b14145
e18184@gmail.com <e18184@gmail.com>
2023-08-14 15:00:32 -0400

    crud menus 14-08-2023

--- src/main/java/com/example/demo/taller1proyecto/controller/MenusController.java
+++ src/main/java/com/example/demo/taller1proyecto/controller/MenusController.java
@@ -2,6 +2,7 @@ package com.example.demo.taller1proyecto.controller;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Objects;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
@@ -9,6 +10,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import com.example.demo.taller1proyecto.modelo.Menus;
+
 import com.example.demo.taller1proyecto.service.MenusService;
 //import com.example.demo.taller1proyecto.service.RolesService;
 
@@ -38,4 +40,70 @@ public class MenusController {
             return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
         }
     }
+
+    @PostMapping("/guardar")
+    public ResponseEntity<Menus> create(@RequestBody Menus menus) {
+        try {
+            Menus menu = new Menus();
+            menu.setNombre(menus.getNombre());
+            menu.setEstado(menus.getEstado());
+
+            menuService.save(menu);
+
+            return new ResponseEntity<>(menu, HttpStatus.OK);
+        } catch (Exception e) {
+            return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+    }
+
+    @PutMapping("/actualizar/{id}")
+    public ResponseEntity<Menus> actualizar(@PathVariable("id") Long id, @RequestBody Menus menus) {
+        try {
+            Menus menu = menuService.findById(id).get();
+
+            if (Objects.nonNull(menu)) {
+
+                menu.setNombre(menus.getNombre());
+                menu.setEstado(menus.getEstado());
+                menuService.save(menu);
+
+            }
+            return new ResponseEntity<Menus>(menu, HttpStatus.OK);
+        } catch (Exception e) {
+
+            return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
+
+        }
+
+    }
+
+    @DeleteMapping("/eliminar/{id}")
+    public ResponseEntity<Menus> eliminar(@PathVariable("id") long id) {
+        try {
+
+            menuService.deleteById(id);
+            return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+        } catch (Exception e) {
+            return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+    }
+
+    @DeleteMapping("/baja/{id}")
+    public ResponseEntity<Menus> baja(@PathVariable("id") long id) {
+        try {
+
+            Menus menu = menuService.findById(id).get();
+            if (Objects.nonNull(menu)) {
+
+                menu.setEstado(0);
+                menuService.save(menu);
+
+            }
+
+            return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+        } catch (Exception e) {
+            return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+    }
+
 }
