1f50b1e2125b27552956e87e4a2c6c772dce95f7
e18184@gmail.com <e18184@gmail.com>
2023-10-30 14:51:40 -0400

    30-10-2023 personas

--- src/main/java/com/example/demo/taller1proyecto/controller/PersonasController.java
+++ src/main/java/com/example/demo/taller1proyecto/controller/PersonasController.java
@@ -1,82 +1,91 @@
 package com.example.demo.taller1proyecto.controller;
 
 import java.util.List;
+import java.util.Optional;
 
+import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.data.domain.Sort;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 import com.example.demo.taller1proyecto.service.PersonasService;
+import com.example.demo.taller1proyecto.service.UsersService;
 import com.example.demo.taller1proyecto.modelo3.Personas;
+import com.example.demo.taller1proyecto.modelo3.Users;
 
 @RestController
 @RequestMapping("/personascontroller")
 public class PersonasController {
+    private final PersonasService personasService;
 
-    PersonasService personasservice;
+    public PersonasController(PersonasService personasService) {
+        this.personasService = personasService;
+    }
 
-    @GetMapping("/")
-    public ResponseEntity<List<Personas>> findAll() {
+    @GetMapping
+    public ResponseEntity<List<Personas>> getPersonas() {
         try {
             Sort sortBy = Sort.by(new Sort.Order(Sort.Direction.ASC, "nombres").ignoreCase());
-            List<Personas> personas = personasservice.findAll(sortBy);
-            for (Personas per : personas) {
-                per.setSolicitudes_servicio(null);
-                per.setTecnicos_ejecutantes(null);
-                per.setUsuarios(null);
-
-            }
+            List<Personas> personas = personasService.findAll(sortBy);
             return new ResponseEntity<>(personas, HttpStatus.OK);
-        } catch (
-
-        Exception e) {
-            return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
-        }
-    }
-
-    // @GetMapping("/{id}")
-    // public ResponseEntity<?> find(@PathVariable Integer id) {
-    // try {
-    // // TODO Implement Your Logic To Get Data From Service Layer Or Directly From
-    // // Repository Layer
-    // return new ResponseEntity<>("GetOne Result", HttpStatus.OK);
-    // } catch (Exception e) {
-    // return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
-    // }
-    // }
-
-    @PostMapping()
-    public ResponseEntity<Personas> create(@RequestBody Personas personas) {
-        try {
-            Personas per = new Personas();
-            per.setApellido_materno(personas.getApellido_materno());
-            per.setApellido_paterno(personas.getApellido_paterno());
-            per.setCedula_identidad(personas.getCedula_identidad());
-            personasservice.save(per);
-            return new ResponseEntity<>(per, HttpStatus.OK);
         } catch (Exception e) {
             return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
         }
     }
 
-    // @PutMapping()
-    // public ResponseEntity<?> update(@RequestBody Dto dto) {
-    // try {
-    // personasservice.save(personasservice.findById());
-    // return new ResponseEntity<>("Update Result", HttpStatus.OK);
-    // } catch (Exception e) {
-    // return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
-    // }
-    // }
+    @GetMapping("/{id}")
+    public ResponseEntity<Personas> findById(@PathVariable Long id) {
+        try {
+            Optional<Personas> personasOptional = personasService.findById(id);
+
+            if (personaOptional.isPresent()) {
+                Personas persona = (Persona) userOptional.get();
+                System.out.println("usuario:" + user.toString());
+                return new ResponseEntity<>(user, HttpStatus.OK);
+            } else {
+                return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+            }
+        } catch (Exception e) {
+            return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+    }
+
+    @PostMapping
+    public ResponseEntity<Users> createUser(@RequestBody Users user) {
+        try {
+            // Long maxId = userService.findMaxId().longValue();
+            // System.out.println("id max es: " + maxId);
+            // Long newId = (maxId != null) ? maxId + 1 : 1;
+            // System.out.println("newId es: " + newId);
+            // user.setId(newId);
+            userService.save(user);
+            return new ResponseEntity<>(user, HttpStatus.CREATED);
+        } catch (DataIntegrityViolationException e) {
+            return ResponseEntity.status(HttpStatus.CONFLICT).build();
+        }
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<Users> updateUser(@PathVariable Long id, @Validated @RequestBody Users user) {
+        Optional<Users> usuarioOpcional = userService.findById(id);
+        if (!usuarioOpcional.isPresent()) {
+            return ResponseEntity.unprocessableEntity().build();
+        }
+        user.setId(usuarioOpcional.get().getId());
+        this.userService.save(user);
+        return ResponseEntity.noContent().build();
+    }
 
     @DeleteMapping("/{id}")
-    public ResponseEntity<?> delete(@PathVariable Integer id) {
-        try {
-            // personasservice.deleteById(id);
-
-            return new ResponseEntity<>("Destroy Result", HttpStatus.OK);
-        } catch (Exception e) {
-            return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
-        }
+    public ResponseEntity<Void> deleteUser(@PathVariable Long id) {
+        userService.deleteById(id);
+        return ResponseEntity.noContent().build();
     }
+
+    @GetMapping("/users")
+    public List<Users> getUsersByQuery(@RequestParam String model, @RequestParam String query) {
+        return userService.getUsersByColumn(model, query);
+    }
+
 }
